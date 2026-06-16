cfed708433bda05399dcfcec8db0de1b38b05525
rofeyev1904-bit <rofeyev_1904@icloud.com>
2026-06-16 11:19:08 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -61,16 +61,16 @@ def photo_handler(message):
 📄 Tajriba: {data['tajriba']}
 """
 
-    for admin_id in ADMIN_IDS:
-        bot.send_message(admin_id, text)
+for admin_id in ADMIN_IDS:
+    bot.send_message(admin_id, text)
 
-    photo_id = message.photo[-1].file_id
+photo_id = message.photo[-1].file_id
 
-    for admin_id in ADMIN_IDS:
-        bot.send_photo(admin_id, photo_id)
+for admin_id in ADMIN_IDS:
+    bot.send_photo(admin_id, photo_id)
 
-        bot.send_message(chat_id, "Anketangiz muvaffaqiyatli qabul qilindi, tez orada siz bilan bog'lanishadi.")
+bot.send_message(chat_id, "Anketangiz muvaffaqiyatli yuborildi!")
 
-             del user_data[chat_id]
+del user_data[chat_id]
 
 bot.infinity_polling()
